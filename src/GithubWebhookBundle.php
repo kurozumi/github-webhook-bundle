76ac48320c06e88c47564c854663c1a2e193ab5e
kurozumi <info@a-zumi.net>
2024-10-07 11:24:59 +0900

    bug fix

--- src/GithubWebhookBundle.php
+++ src/GithubWebhookBundle.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kurozumi\GithubWebhookBundle;
 
+use Kurozumi\GithubWebhookBundle\DependencyInjection\GithubExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GithubWebhookBundle extends Bundle
@@ -11,5 +13,10 @@ class GithubWebhookBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new GithubExtension();
     }
 }
